3673e78f05a8d1e1960e8a4ef17c1122ef0c870c
Oscar_Ding <dinggaozan@126.com>
2020-06-23 13:24:34 +0800

    20200623 13:24 Oscar

--- 07_Ensemble.py
+++ 07_Ensemble.py
@@ -1,21 +1,28 @@
-from scipy.special import comb
 import math
-import numpy as np
+from itertools import product
+
 import matplotlib.pyplot as plt
-from ensemble import MajorityVoteClassifier
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.model_selection import train_test_split, cross_val_score
+import numpy as np
+import pandas as pd
+from scipy.special import comb
 from sklearn import datasets
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_curve, auc, accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+
+from ensemble import MajorityVoteClassifier
 
 author = "Oscar Ding"
 
 """
 majority vote
 """
+
 
 def ensemble_error(n_classifier, error):
     k_start = int(math.ceil(n_classifier / 2.))
@@ -78,7 +85,287 @@ for clf, label in zip([pipe1, clf2, pipe3], clf_labels):
     print("ROC AUC: %0.2f (+/- %0.2f) [%s]"
           % (scores.mean(), scores.std(), label))
 
+# Majority Rule (hard) Voting
+mv_clf = MajorityVoteClassifier(classifiers=[pipe1, clf2, pipe3])
 
+clf_labels += ['Majority voting']
+all_clf = [pipe1, clf2, pipe3, mv_clf]
 
+for clf, label in zip(all_clf, clf_labels):
+    scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, n_jobs=4, scoring='roc_auc')
+    print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
 
+# Evaluating and tuning the ensemble classifier
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
 
+for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    # assuming the label of the positive class is 1
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr,
+             color=clr,
+             linestyle=ls,
+             label='%s (auc = %0.2f)' % (label, roc_auc))
+
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1],
+         linestyle='--',
+         color='gray',
+         linewidth=2)
+
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid(alpha=0.5)
+plt.xlabel('False positive rate (FPR)')
+plt.ylabel('True positive rate (TPR)')
+plt.show()
+
+# check decision regions
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+
+all_clf = [pipe1, clf2, pipe3, mv_clf]
+
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(nrows=2, ncols=2,
+                        sharex='col',
+                        sharey='row',
+                        figsize=(7, 5))
+
+for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+    clf.fit(X_train_std, y_train)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train == 0, 0],
+                                  X_train_std[y_train == 0, 1],
+                                  c='blue',
+                                  marker='^',
+                                  s=50)
+
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train == 1, 0],
+                                  X_train_std[y_train == 1, 1],
+                                  c='green',
+                                  marker='o',
+                                  s=50)
+
+    axarr[idx[0], idx[1]].set_title(tt)
+
+plt.text(-3.5, -5.,
+         s='Sepal width [standardized]',
+         ha='center', va='center', fontsize=12)
+plt.text(-3, 4.5,
+         s='Petal length [standardized]',
+         ha='center', va='center',
+         fontsize=12, rotation=90)
+plt.show()
+
+# get parameters from each individual classifier for grid search
+mv_clf.get_params()
+
+params = {'decisiontreeclassifier__max_depth': [1, 2],
+          'pipeline-1__clf__C': [0.001, 0.1, 100.0]}
+
+grid = GridSearchCV(estimator=mv_clf,
+                    param_grid=params,
+                    cv=10,
+                    n_jobs=4,
+                    scoring='roc_auc',
+                    iid=False)
+
+grid.fit(X_train, y_train)
+
+for r, _ in enumerate(grid.cv_results_['mean_test_score']):
+    print("%0.3f +/- %0.2f %r"
+          % (grid.cv_results_['mean_test_score'][r],
+             grid.cv_results_['std_test_score'][r] / 2.0,
+             grid.cv_results_['params'][r]))
+
+print('Best parameters: %s' % grid.best_params_)
+print('Accuracy: %.2f' % grid.best_score_)
+# Best parameters: {'decisiontreeclassifier__max_depth': 1, 'pipeline-1__clf__C': 100.0}
+# Accuracy: 0.98
+
+grid.best_estimator_.classifiers
+
+# set best parameters
+mv_clf.set_params(**grid.best_estimator_.get_params())
+
+mv_clf
+
+"""
+Bagging -- Building an ensemble of classifiers from bootstrap samples
+"""
+# bootstrap samples (random samples with replacement)
+# we want to perform bagging on an ensemble of classifiers with low bias,
+# for example, unpruned decision trees
+
+# Applying bagging to classify samples in the Wine dataset
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                      'machine-learning-databases/wine/wine.data',
+                      header=None)
+
+df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium', 'Total phenols',
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanins',
+                   'Color intensity', 'Hue', 'OD280/OD315 of diluted wines',
+                   'Proline']
+
+# drop 1 class
+df_wine = df_wine[df_wine['Class label'] != 1]
+y = df_wine['Class label'].values
+X = df_wine[['Alcohol', 'OD280/OD315 of diluted wines']].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
+
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=None, random_state=1)
+
+bag = BaggingClassifier(base_estimator=tree,
+                        n_estimators=500,
+                        max_samples=1.0,
+                        max_features=1.0,
+                        bootstrap=True,
+                        bootstrap_features=False,
+                        n_jobs=4,
+                        random_state=1)
+
+# calculate accuracy score
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f'
+      % (tree_train, tree_test))
+
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print('Bagging train/test accuracies %.3f/%.3f'
+      % (bag_train, bag_test))
+
+# plot decision regions
+x_min = X_train[:, 0].min() - 1
+x_max = X_train[:, 0].max() + 1
+y_min = X_train[:, 1].min() - 1
+y_max = X_train[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(nrows=1, ncols=2,
+                        sharex='col',
+                        sharey='row',
+                        figsize=(8, 3))
+
+for idx, clf, tt in zip([0, 1],
+                        [tree, bag],
+                        ['Decision tree', 'Bagging']):
+    clf.fit(X_train, y_train)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train == 0, 0],
+                       X_train[y_train == 0, 1],
+                       c='blue', marker='^')
+
+    axarr[idx].scatter(X_train[y_train == 1, 0],
+                       X_train[y_train == 1, 1],
+                       c='green', marker='o')
+
+    axarr[idx].set_title(tt)
+
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(10.2, -0.5,
+         s='OD280/OD315 of diluted wines',
+         ha='center', va='center', fontsize=12)
+
+plt.tight_layout()
+plt.show()
+
+"""
+Leveraging weak learners via adaptive boosting
+"""
+# The key concept behind boosting is to focus on training samples that are hard to classify, that is,
+# to let the weak learners subsequently learn from misclassified training samples to
+# improve the performance of the ensemble.
+tree = DecisionTreeClassifier(criterion='entropy',
+                              max_depth=1,
+                              random_state=1)
+
+ada = AdaBoostClassifier(base_estimator=tree,
+                         n_estimators=500,
+                         learning_rate=0.1,
+                         random_state=1)
+
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f'
+      % (tree_train, tree_test))
+
+ada = ada.fit(X_train, y_train)
+y_train_pred = ada.predict(X_train)
+y_test_pred = ada.predict(X_test)
+
+ada_train = accuracy_score(y_train, y_train_pred)
+ada_test = accuracy_score(y_test, y_test_pred)
+print('AdaBoost train/test accuracies %.3f/%.3f'
+      % (ada_train, ada_test))
+
+# plot decision regions
+
+x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
+y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                     np.arange(y_min, y_max, 0.1))
+
+f, axarr = plt.subplots(1, 2, sharex='col', sharey='row', figsize=(8, 3))
+
+for idx, clf, tt in zip([0, 1],
+                        [tree, ada],
+                        ['Decision tree', 'AdaBoost']):
+    clf.fit(X_train, y_train)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    axarr[idx].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx].scatter(X_train[y_train == 0, 0],
+                       X_train[y_train == 0, 1],
+                       c='blue', marker='^')
+    axarr[idx].scatter(X_train[y_train == 1, 0],
+                       X_train[y_train == 1, 1],
+                       c='green', marker='o')
+    axarr[idx].set_title(tt)
+
+axarr[0].set_ylabel('Alcohol', fontsize=12)
+plt.text(10.2, -0.5,
+         s='OD280/OD315 of diluted wines',
+         ha='center', va='center', fontsize=12)
+
+plt.tight_layout()
+plt.show()
